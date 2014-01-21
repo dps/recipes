37e53acb28cca4d8e564f630ad614c81e50f42ae
David Singleton <davidsingleton@gmail.com>
2014-01-20 21:22:16 -0800

    fix paths

--- validate/test_validate_recipes.py
+++ validate/test_validate_recipes.py
@@ -2,9 +2,9 @@ import os
 from recipemarkdown import parse_recipe_markdown
 
 def test_validate_recipes():
-  for filename in os.listdir('..'):
+  for filename in os.listdir('.'):
     if filename.startswith('recipe'):
-      rf = file('..' + os.sep + filename, 'r')
+      rf = file('.' + os.sep + filename, 'r')
       data = ''.join(rf.readlines())
       res = parse_recipe_markdown(data, {}, 'http://test/')
       if res.has_key('error'):
